bef7c96aeef4418a61b7b65a4e77315d2647ce0a
whatswitheffy <48794170+whatswitheffy@users.noreply.github.com>
2020-10-20 22:51:46 +0600

    Update main.c

--- sources/main.c
+++ sources/main.c
@@ -12,11 +12,9 @@ char *getWord(char *end) {
     int i = 0;
     char ch;
     char *word = NULL;
-
     if (*end == '\n') {
         return NULL;
     }
-
     ch = getchar();
 
     if (ch == '\n') {
@@ -80,25 +78,21 @@ void pipeForTwo(char **list, int iForP) {
     int fdForP[2];
     int i, k, j, p;
     char **cmd_A, **cmd_B;
-
     cmd_A = (char **)malloc((iForP + 1) * sizeof(char*));
     for (i = 0; i < iForP; i++) {
         cmd_A[i] = list[i];
     }
-
     cmd_A[i] = NULL;
     i = iForP + 1;
     while (list[i] != NULL) {
         i++;
     }
-
     cmd_B = (char **)malloc((i - iForP) * sizeof(char*));
     k = 0;
     for (j = iForP + 1; j < i;j++) {
         cmd_B[k] = list[j];
         k++; 
     }
-
     cmd_B[k] = NULL;
     list[iForP] = NULL;
     iForP = 0;
@@ -137,9 +131,7 @@ char ***getCmdArr(char **list, int n) {
         cmdArr[j][i] = list[i + lstPipePos];
     }
     cmdArr[j][pipePosition - lstPipePos] = NULL;
-
     return cmdArr;
-
 }
 
 void runManyCommands(char **list, int *iForP, int n) {
@@ -177,14 +169,12 @@ int *background_pids = NULL;
 int count_background = 0; 
 
 int flow(char **list) {
-    int flag = 0;
-    int fd = 0;
-    int i = 0;
-    int iForP = 0;
+    int flag = 0, fd = 0, i = 0, iForP = 0, n = 0;
     int pid;
     int background = 0;
-    int n = 0;
-
+    char * getenv (const char * name ); 
+    int setenv (const char * name, const char * value, int overwrite);
+    const char *home = getenv("HOME");
     while (list[i] != NULL) {
         if (strcmp(list[i], "<") == 0) {
             fd = open(list[i + 1], O_RDONLY);
@@ -214,10 +204,17 @@ int flow(char **list) {
             free(list[i]);
             list[i] = NULL;
             break;
-        }
+        } else if (strcmp(list[0] , "cd") == 0) {
+            if (list[1] == NULL || strcmp (list[1] , "~" ) == 0) {
+                chdir (home);
+                return 0;
+            } else {
+                chdir (list[1]);
+                return 0;
+            }
+            }
         i++;
     }
-
     if (iForP != 0) {
         runManyCommands(list, &iForP, n);
     } else {
@@ -241,16 +238,13 @@ int flow(char **list) {
             return 1;
         }
     }
-
     if (fd) {
         close(fd);
     }
     fd = 0;
 }
 
-int main(void) {
-    
-
+int main(void) {    
     printf("SUPER EVA'S TRMNAL >>");
     char **list = getList();
     while (!isExit(list)) {
@@ -259,9 +253,7 @@ int main(void) {
         freeList(list);
         list = getList();
     }
-    
     freeList(list);
-
     for (int i = 0; i < count_background; i++) {
         waitpid(background_pids[i], NULL, 0);
     }
